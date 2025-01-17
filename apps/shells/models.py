f9e6becc3e2b6f25e32d40a6467d287569a4232e
Fares Emad <faresemadx@gmail.com>
2025-01-17 23:42:27 +0200

    feat: :sparkles: Update Shell model and admin interface to include SSL, TLD, and details fields; modify TLD field to allow null and blank values

--- apps/shells/models.py
+++ apps/shells/models.py
@@ -27,7 +27,7 @@ class Shell(models.Model):
     status = models.CharField(max_length=20, choices=ShellStatus.choices, default=ShellStatus.UNSOLD)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     ssl = models.BooleanField(default=False)
-    tld = models.CharField(max_length=10, default="com")
+    tld = models.CharField(max_length=10, null=True, blank=True)
     details = models.JSONField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

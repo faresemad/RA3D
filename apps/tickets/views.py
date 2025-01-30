3b14bd41cc8953e7152f41c8ae55064021fa3b7c
Fares Emad <faresemadx@gmail.com>
2025-01-30 20:32:20 +0200

    feat: :sparkles: Refactor TicketViewSet and add TicketResponseViewSet for improved ticket management and response handling

--- apps/tickets/views.py
+++ apps/tickets/views.py
@@ -1,64 +1,53 @@
+# views.py
 import logging
 
-from django.db.models import Q
-from rest_framework import mixins, status, viewsets
+from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
-from apps.tickets.models import Ticket
-from apps.tickets.serializers import CreateTicketSerializer, ListTicketSerializer, RetrieveTicketSerializer
-from apps.utils.permissions import IsOwnerOrAdmin, IsSupport
+from .models import Ticket, TicketResponse
+from .permissions import IsOwnerOrStaff, IsTicketParticipantOrStaff
+from .serializers import TicketResponseSerializer, TicketSerializer
 
 logger = logging.getLogger(__name__)
 
 
-class TicketViewSet(
-    mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet
-):
-    queryset = Ticket.objects.select_related("user").all()
-    serializer_class = ListTicketSerializer
-    permission_classes = [IsAuthenticated]
+class TicketViewSet(viewsets.ModelViewSet):
+    serializer_class = TicketSerializer
+    permission_classes = [IsOwnerOrStaff]
 
-    def get_serializer_class(self):
-        if self.action == "create":
-            return CreateTicketSerializer
-        if self.action == "retrieve":
-            return RetrieveTicketSerializer
-        return super().get_serializer_class()
-
-    def get_permissions(self):
-        if self.action == "create":
-            return [IsAuthenticated()]
-        if self.action == "close":
-            return [Q(IsAdminUser()) | Q(IsSupport())]
-        if self.action == "destroy":
-            return [IsOwnerOrAdmin()]
-        return super().get_permissions()
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Ticket.objects.all()
+        return Ticket.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        super().perform_create(serializer)
-        logger.info(f"Ticket created by {self.request.user.email}")
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        serializer.save(user=self.request.user)
+        logger.info(f"Ticket {serializer.instance.id} created by {self.request.user}")
 
     @action(detail=True, methods=["post"])
     def close(self, request, pk=None):
         ticket = self.get_object()
         ticket.mark_as_closed()
-        logger.info(f"Ticket closed by {self.request.user.email}")
-        return Response(status=status.HTTP_200_OK)
+        logger.info(f"Ticket {ticket.id} closed by {request.user}")
+        return Response({"status": "Ticket closed"})
 
-    def perform_destroy(self, instance):
-        super().perform_destroy(instance)
-        logger.info(f"Ticket deleted by {self.request.user.email}")
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    @action(detail=True, methods=["post"])
+    def reopen(self, request, pk=None):
+        ticket = self.get_object()
+        ticket.mark_as_opened()
+        logger.info(f"Ticket {ticket.id} reopened by {request.user}")
+        return Response({"status": "Ticket reopened"})
 
-    @action(detail=False, methods=["get"])
-    def my_ticket(self, request, pk=None):
-        queryset = self.get_queryset().filter(user=request.user)
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+
+class TicketResponseViewSet(viewsets.ModelViewSet):
+    serializer_class = TicketResponseSerializer
+    permission_classes = [IsTicketParticipantOrStaff]
+
+    def get_queryset(self):
+        return TicketResponse.objects.filter(ticket_id=self.kwargs["ticket_pk"])
+
+    def perform_create(self, serializer):
+        ticket = Ticket.objects.get(pk=self.kwargs["ticket_pk"])
+        serializer.save(user=self.request.user, ticket=ticket)
+        logger.info(f"Response added to ticket {ticket.id} by {self.request.user}")

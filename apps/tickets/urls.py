9769748b9433bf1dc239bcda038c9811ae571721
Fares Emad <faresemadx@gmail.com>
2025-01-30 20:44:39 +0200

    feat: :sparkles: Add nested routing for TicketResponseViewSet and update ticket URLs

--- apps/tickets/urls.py
+++ apps/tickets/urls.py
@@ -1,10 +1,16 @@
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 
-from apps.tickets.views import TicketViewSet
+from apps.tickets.views import TicketResponseViewSet, TicketViewSet
 
 router = DefaultRouter()
 
 app_name = "tickets"
 
-router.register(r"", TicketViewSet, basename="tickets")
-urlpatterns = router.urls
+router.register(r"tickets", TicketViewSet, basename="tickets")
+
+tickets_router = routers.NestedSimpleRouter(router, r"tickets", lookup="ticket")
+tickets_router.register(r"responses", TicketResponseViewSet, basename="ticket-responses")
+
+
+urlpatterns = router.urls + tickets_router.urls

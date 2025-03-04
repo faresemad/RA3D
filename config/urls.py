75da455e2d67f743c3c89a07ad2f64906d2949f9
Fares Emad <faresemadx@gmail.com>
2025-03-04 17:30:04 +0200

    feat: :sparkles: remove leads app and associated files from the project

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,6 @@ urlpatterns += [
     path(f"{settings.API_PREFIX}shells/", include("apps.shells.urls", namespace="shells")),
     path(f"{settings.API_PREFIX}rdp/", include("apps.rdp.urls", namespace="rdp")),
     path(f"{settings.API_PREFIX}cpanel/", include("apps.cpanel.urls", namespace="cpanel")),
-    path(f"{settings.API_PREFIX}leads/", include("apps.leads.urls", namespace="leads")),
     path(f"{settings.API_PREFIX}notifications/", include("apps.notifications.urls", namespace="notifications")),
     path(f"{settings.API_PREFIX}orders/", include("apps.orders.urls", namespace="orders")),
     path(f"{settings.API_PREFIX}wallet/", include("apps.wallet.urls", namespace="wallet")),

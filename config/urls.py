95f1d66eac812127e3e7562621c2b142fcc7c9fe
Fares Emad <faresemadx@gmail.com>
2025-04-02 16:36:22 +0200

    feat: Add catch-all URL and serve index.html for root
    This commit adds a catch-all URL pattern to serve the `index.html` template for any invalid URLs, improving the user experience by preventing 404 errors. It also serves `index.html` at the root path ("/") of the application.

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 # API patterns for Admin
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
+    path("", TemplateView.as_view(template_name="index.html")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API patterns for Local Apps
@@ -37,3 +39,8 @@ urlpatterns += [
 
 if settings.DEBUG:
     urlpatterns += (path("__debug__/", include("debug_toolbar.urls")),)
+
+# Catch-all pattern for invalid URLs
+urlpatterns += [
+    path("<path:invalid_path>", TemplateView.as_view(template_name="index.html"), name="404"),
+]

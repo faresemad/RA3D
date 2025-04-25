c99399f1a97fceadf3724a1ec75505ea28fc808a
Fares Emad <faresemadx@gmail.com>
2025-04-25 19:28:41 +0300

    feat: :sparkles: Configure periodic task to cancel expired orders.
    This schedule runs the 'cancel_expired_orders' task from the orders app every 15 minutes to automatically cancel orders that have exceeded their expiration time.

--- config/celery_app.py
+++ config/celery_app.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.beat import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
@@ -15,3 +16,10 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "cancel-expired-orders": {
+        "task": "apps.orders.tasks.cancel_expired_orders",
+        "schedule": crontab(minute="*/15"),
+    },
+}

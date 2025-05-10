cfd54b9bf96159bf3433eaf60873d5e5f64358f4
Fares Emad <faresemadx@gmail.com>
2025-05-10 03:22:09 +0300

    fix: Update user status to pending upon SellerRequest creation
    This commit updates the user's account status to "pending seller" when a new SellerRequest is created. This ensures that the user's status is correctly set to pending review by an administrator.
    
    The changes include:
    
    - Modified the `create_seller_profile` signal handler to update the user's status to `PENDING_SELLER` upon `SellerRequest` creation.
    - Removed the conditional logic that was previously setting the user's status based on the `SellerRequest` status, as this logic is now handled elsewhere.
    - Added logging to indicate when the user status is being updated to pending.

--- apps/sellers/signals.py
+++ apps/sellers/signals.py
@@ -1,18 +1,17 @@
+import logging
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from apps.sellers.models import SellerRequest
 from apps.users.models import CustomUserProfile
 
+logger = logging.getLogger(__name__)
+
 
 @receiver(post_save, sender=SellerRequest)
-def create_seller_profile(sender, instance: SellerRequest, created, **kwargs):
+def update_user_status_to_pending(sender, instance, created, **kwargs):
     if created:
-        user: CustomUserProfile = instance.user
-        match instance.status:
-            case SellerRequest.Status.PENDING:
-                user.make_pending_seller()
-            case (SellerRequest.Status.APPROVED):
-                user.make_seller()
-            case (SellerRequest.Status.REJECTED):
-                user.make_buyer()
+        logger.info("Updating user status to pending")
+        instance.user.status = CustomUserProfile.AccountStatus.PENDING_SELLER
+        instance.user.save()

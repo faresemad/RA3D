9f8af6525a8ea4286ec3fe013d62a057a1c1419c
Fares Emad <faresemadx@gmail.com>
2025-05-16 23:27:43 +0300

    fix: Add type hint to instance parameter in update_user_status_to_pending signal
    This commit adds a type hint to the `instance` parameter in the `update_user_status_to_pending` signal receiver function. This improves code readability and helps with static analysis.
    
    The change includes:
    
    - Adding a type hint `instance: SellerRequest` to the `instance` parameter in the `update_user_status_to_pending` function in `apps/sellers/signals.py`.

--- apps/sellers/signals.py
+++ apps/sellers/signals.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=SellerRequest)
-def update_user_status_to_pending(sender, instance, created, **kwargs):
+def update_user_status_to_pending(sender, instance: SellerRequest, created, **kwargs):
     if created:
         logger.info("Updating user status to pending")
         instance.user.status = CustomUserProfile.AccountStatus.PENDING_SELLER
